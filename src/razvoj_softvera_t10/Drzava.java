9e8c64ccbade3c5cfc3364b70392ab4bf5cfe008
mgojak1 <mgojak1@etf.unsa.ba>
2019-05-27 09:26:36 +0200

    init

---
+++ src/razvoj_softvera_t10/Drzava.java
@@ -0,0 +1,9 @@
+package razvoj_softvera_t10;
+
+public class Drzava {
+    private String naziv, jedinica_za_povrsinu;
+    private int broj_stanovnika;
+    private  double povrsina;
+    private Grad glavni_grad;
+
+}
